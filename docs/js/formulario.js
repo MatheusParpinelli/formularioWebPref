f9c4b2c808e33e8f6b5bf266d429734d8b52f95b
Matheus Parpinelli <111715476+MatheusParpinelli@users.noreply.github.com>
2023-04-19 16:00:33 -0300

    Update formulario.js

--- docs/js/formulario.js
+++ docs/js/formulario.js
@@ -1,17 +1,14 @@
 function enviar() {
-    // Obter os valores inseridos pelo usuário
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var assunto = document.getElementById("assunto").value;
-    var mensagem = document.getElementById("mensagem").value;
     
-    // Criar um texto com as seleções do usuário
-    var texto = "Nome: " + nome + "\nE-mail: " + email + "\nAssunto: " + assunto + "\nMensagem: " + mensagem;
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let assunto = document.getElementById("assunto").value;
+    let mensagem = document.getElementById("mensagem").value;
     
-    // Exibir o texto na janela de alerta
+    let texto = "Nome: " + nome + "\nE-mail: " + email + "\nAssunto: " + assunto + "\nMensagem: " + mensagem;
+    
     alert(texto);
     
-    // Alterar as cores com base nas seleções do usuário
     if (assunto == "duvida") {
         document.body.style.backgroundColor = "#ff0000";
         document.body.style.color = "#000000";
